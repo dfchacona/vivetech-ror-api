7c2b12f0edf2339dddf39cf3786f2b87b9b0eb72
dieguischa <dfchacona@unal.edu.co>
2022-02-11 11:32:35 -0500

    Updated method

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,22 +7,17 @@ class Product < ApplicationRecord
   def self.create_products(products)
     successful_products = 0
     products.each do |new_product|
-      existing_product = Product.where(["name = ?", new_product[:name]])
-      if existing_product
-        update_product(existing_product)
-      else
-        product = Product.new(name: new_product[:name], description: new_product[:description])
-        if product.valid?
-          if new_product[:variants]
-            new_product[:variants].each do |variant|
-              new_variant = Variant.new(name: variant[:name], price: variant[:price])
-              product.variants << new_variant
-            end
+      product = Product.new(name: new_product[:name], description: new_product[:description])
+      if product.valid?
+        if new_product[:variants]
+          new_product[:variants].each do |variant|
+            new_variant = Variant.new(name: variant[:name], price: variant[:price])
+            product.variants << new_variant
           end
-          unless product.variants.empty?
-            if product.save
-              successful_products += 1
-            end
+        end
+        unless product.variants.empty?
+          if product.save
+            successful_products += 1
           end
         end
       end
